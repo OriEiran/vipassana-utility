aee42d787ce627475d33cb046309a05d4898f4f3
OriEiran <ori.eiran@gmail.com>
2025-02-05 16:16:09 +0200

    reverted

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,23 @@
 const CACHE_NAME = 'meditation-app-v1';
 const assetsToCache = [
   '/',
-  '/index.html',
+  '/index.ejs',
   '/Goenka.mp3',
-  '/Goenka45.mp3',
+  '/Goenka45mp3',
   '/manifest.json',
-  '/public/icons/icon-192.png',
-  '/public/icons/icon-512.png',
-  '/service-worker.js'
+  // Add other assets
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache opened');
-        return cache.addAll(assetsToCache);
-      })
+      .then(cache => cache.addAll(assetsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Return cached version if available
-        if (response) {
-          return response;
-        }
-        // Otherwise fetch from network
-        return fetch(event.request)
-          .then(response => {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            
-            // Clone the response as it can only be consumed once
-            const responseToCache = response.clone();
-            
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-              
-            return response;
-          });
-      })
+      .then(response => response || fetch(event.request))
   );
 });
